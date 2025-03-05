8f37c2727eb854355f03751a583c801a32fcb1c5
DiverseBloom780 <98516443+DiverseBloom780@users.noreply.github.com>
2025-03-04 20:17:17 -0600

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,158 @@
+import asyncio
+import re
+import httpx
+from selectolax.lexbor import LexborHTMLParser
+
+
+class Scraper:
+    def __init__(self):
+        self.base_url = "https://getcomics.org"
+        self.url = None
+        self.session = httpx.AsyncClient()
+
+    def set_search_url(self, query, page):
+        self.url = f"{self.base_url}/page/{page}/?s={query}"
+
+    def set_latestpage_url(self):
+        self.url = self.base_url
+
+    def set_tag_url(self, tag, page):
+        self.url = f"{self.base_url}/cat/{tag}/"
+        if page > 1:
+            self.url += f"page/{page}/"
+
+    async def get_search_results(self):
+        try:
+            response = await self.session.get(self.url)
+            # Check if the page exists (status code 404 means the page is not found)
+            if response.status_code == 404:
+                print(f"Page not found: {self.url}")
+                return 404
+
+            articles = await self.extract_articles()
+            if isinstance(articles, int):
+                return articles
+
+            return await self.create_metadata(articles)
+
+        except httpx.RequestError as exc:
+            print(f"Request failed for {self.url}: {exc}")
+            return 500  # General failure code
+
+    async def extract_articles(self):
+        try:
+            response = await self.session.get(self.url)
+            parser = LexborHTMLParser(response.text)
+            articles = parser.css_first("div.post-list-posts")
+
+            if not articles:
+                print(f"No articles found on {self.url}")
+                return 201  # No articles found
+
+            articles = articles.css("article")
+            if self.url == self.base_url:
+                articles = self.remove_ads(articles)
+
+            return articles
+
+        except httpx.RequestError as exc:
+            print(f"Error fetching articles from {self.url}: {exc}")
+            return 500  # General failure code
+
+    def remove_ads(self, articles: list):
+        return [
+            article
+            for article in articles
+            if "post-145619" not in article.attributes["id"]
+            and "post-351441" not in article.attributes["id"]
+        ]
+
+    async def create_metadata(self, articles):
+        data = {"Meta-Data": []}
+        description_extraction_coroutines = self.create_couroutines(
+            articles, self.extract_comic_description
+        )
+        download_links_extraction_coroutines = self.create_couroutines(
+            articles, self.extract_download_links
+        )
+
+        description_list = await asyncio.gather(*description_extraction_coroutines)
+        download_links = await asyncio.gather(*download_links_extraction_coroutines)
+
+        for index, article in enumerate(articles):
+            comic_title = article.css_first("h1").css_first("a").text()
+            cover_url = article.css_first("img").attributes["src"]
+
+            search_paragraph = article.css("p")[1].text()
+
+            publish_year = self.search_pattern(r"\b\d{4}(?:-\d{4})?\b", search_paragraph)
+            file_size = self.search_pattern(r"(\d+\.\d+|\d+)( GB| MB)", search_paragraph)
+
+            data["Meta-Data"].append(
+                {
+                    "Title": comic_title,
+                    "Year": publish_year,
+                    "Cover": cover_url,
+                    "Size": file_size,
+                    "Description": description_list[index],
+                    "DownloadLinks": download_links[index],
+                }
+            )
+
+        return data
+
+    def create_couroutines(self, articles, coroutine_function):
+        return [
+            asyncio.ensure_future(coroutine_function(article.css_first("a").attributes["href"]))
+            for article in articles
+        ]
+
+    async def extract_comic_description(self, url):
+        try:
+            response = await self.session.get(url)
+            parser = LexborHTMLParser(response.text)
+            return parser.css_first("section.post-contents").css_first("p").text()
+
+        except httpx.RequestError as exc:
+            print(f"Error fetching description from {url}: {exc}")
+            return None
+
+    async def extract_download_links(self, url):
+        download_links = []
+
+        try:
+            response = await self.session.get(url)
+            parser = LexborHTMLParser(response.text)
+            download_buttons = parser.css("div.aio-pulse")
+
+            if not download_buttons:
+                return None
+
+            for link in download_buttons:
+                download_links.append(link.css_first("a").attributes["href"])
+
+            return download_links
+
+        except httpx.RequestError as exc:
+            print(f"Error fetching download links from {url}: {exc}")
+            return None
+
+    def search_pattern(self, pattern, text):
+        pattern_object = re.compile(pattern)
+        result = re.search(pattern_object, text)
+
+        if not result:
+            return "-"
+        return result.group()
+
+
+# Example run function to start the scraper
+async def main():
+    scraper = Scraper()
+    scraper.set_search_url("batman", 1)
+    results = await scraper.get_search_results()
+    print(results)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
